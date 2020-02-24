3e35178024751bebb133c35e4b0ec6648d5db954
Shivum <shivumkumar1@gmail.com>
2020-02-23 20:50:34 -0600

    Post Exercise

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -10,31 +10,31 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Shrek!"};
-            var customers = new List<Customer>
-            {
-                new Customer() {Name = "Customer 1"},
-                new Customer() {Name = "Customer 2"},
-
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-            return View(viewModel);
-
+            return View(GetMovies());
         }
+        // GET: Movies
+
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie() {Name = "Shrek"},
+                new Movie() {Name = "Wall-E"},
+
+            };
+        }
+
 
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
         }
+
 
     }
 }
